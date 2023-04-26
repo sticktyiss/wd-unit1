f8d2b69a5477055781d3351ab7539027b5cc9b4d
Tyiss Goodman <sticktyiss@gmail.com>
2023-04-26 16:38:22 -0700

    Added square component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+import Square from "./Square";
+// import { useState } from 'react';
 
 function App() {
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
+
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""])
+    setPlayer(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {squares.map((value, index) => {
+          return (
+            <Square
+              squares={squares}
+              setSquares={setSquares}
+              player={player}
+              setPlayer={setPlayer}
+              squareValue={value}
+              index={index}
+            />
+          );
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
     </div>
   );
 }
